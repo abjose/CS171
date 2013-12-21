e2db95104a2f01c0ad5b09a1a6824b66aec0ecf3
Alex Jose <ajose@caltech.edu>
2013-12-20 20:20:03 -0800

    update makefile and change from 'wireframe' to 'shaded'

--- set3/shaded.cc
+++ set3/shaded.cc
@@ -1,22 +1,28 @@
 #include <iostream>
-#include "wireframe.hh"
+#include "shaded.hh"
 #include "matrix/canvas.hh"
+#include "raster/raster.h"
 
 int main(int argc, char *argv[]) {
   // read cmd line args
   int xres, yres;
   //xres = std::stoi(argv[1]); yres = std::stoi(argv[2]); 
   xres = 400; yres = 400;
+  float xMin = -1; float yMin = -1;
+  float xMax =  1; float yMax =  1;
+  
+  // call initDraw for use of raster later
+  initDraw(xMin, xMax, yMin, yMax, xres, yres);
 
   // make a new canvas and scene for the parser
-  Canvas c(-1, -1, 1, 1, xres, yres);
+  //Canvas c(xMin, yMin, xMax, yMax, xres, yres);
   //Canvas c(-5, -5, 5, 5, xres, yres);
   std::unique_ptr<SceneBlock> scene;
   scene = parse_test(std::cin);
 
   // render to the canvas
   //scene->display();
-  scene->render(c);
+  scene->render();//c);
 
   // spit the ppm to std output
   //std::cout << c.to_ppm() << std::endl;
--- set3/shaded.hh
+++ set3/shaded.hh
@@ -1,9 +1,9 @@
-#ifndef __DRAW2D_H_GUARD__
-#define __DRAW2D_H_GUARD__
+#ifndef __SHADED_H_GUARD__
+#define __SHADED_H_GUARD__
 
 #include <memory>
 #include "blocks/scene_block.hh"
 
 std::unique_ptr<SceneBlock> parse_test(std::istream &datafile);
 
-#endif // __DRAW2D_H_GUARD__
+#endif // __SHADED_H_GUARD__
