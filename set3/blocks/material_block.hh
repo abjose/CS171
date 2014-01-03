035313c011e0f85242d07e3308343afb039d918f
Alex B. Jose <ajose@caltech.edu>
2014-01-03 12:23:07 -0800

    enforce constraints from set

--- set3/blocks/material_block.hh
+++ set3/blocks/material_block.hh
@@ -1,6 +1,8 @@
 #ifndef __MATERIAL_BLOCK_H_GUARD__
 #define __MATERIAL_BLOCK_H_GUARD__
 
+#include <algorithm>
+
 #include "../matrix/matrix.hh"
 #include "../matrix/transform.hh"
 
@@ -14,13 +16,32 @@ public:
   //public:
   float shininess;
 
+  MaterialBlock() {
+    set_ambient(.2,.2,.2);
+    set_diffuse(.8,.8,.8);
+    set_specular(0,0,0);
+    shininess = 0.2;
+  }
+
   void set_ambient(float r, float g, float b) {
+    // clamp values
+    r = std::max(0.f, std::min(1.f, r));
+    g = std::max(0.f, std::min(1.f, g));
+    b = std::max(0.f, std::min(1.f, b));
     ambient = makeVector3<float>(r,g,b);
   }
   void set_diffuse(float r, float g, float b) {
+    // clamp values
+    r = std::max(0.f, std::min(1.f, r));
+    g = std::max(0.f, std::min(1.f, g));
+    b = std::max(0.f, std::min(1.f, b));
     diffuse = makeVector3<float>(r,g,b);
   }
   void set_specular(float r, float g, float b) {
+    // clamp values
+    r = std::max(0.f, std::min(1.f, r));
+    g = std::max(0.f, std::min(1.f, g));
+    b = std::max(0.f, std::min(1.f, b));
     specular = makeVector3<float>(r,g,b);
   }
 
