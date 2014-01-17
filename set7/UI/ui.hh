d669b3a1c4853999dba2b786894f349c919aa2c4
Alex B. Jose <ajose@caltech.edu>
2014-01-17 02:29:21 -0800

    clean up ui

--- set7/UI/ui.hh
+++ set7/UI/ui.hh
@@ -20,57 +20,7 @@ private:
   // global
   std::shared_ptr<Framer> f;
 
- 
-
-  // TRANSLATION
-  // state variables - set to 0 if off, 1 if on
-  int is_translate = 0;
-  int is_zoom = 0;
-  int is_rotate = 0;
-  // constants for translate and zoom normals
-  float TC = 0.005;
-  float ZC = 0.005;
-  float RC = 0.05;
-  // x,y at which translation was started
-  int init_tx = 0;
-  int init_ty = 0;
-  // y at which zoom was started
-  int init_zy = 0;
-  // x,y at which rotation was started
-  int init_rx = 0;
-  int init_ry = 0;
-  // amount to translate in PIXELS...worry about overflow?
-  int translate_x = 0;
-  int translate_y = 0;
-  int translate_z = 0;
-  // rotation stuff
-  int rotate_d = 0;
-  int rotate_x = 0;
-  int rotate_y = 0;
-  int rotate_z = 0;
-
-  // ROTATION
-
-
-  // transform functions
-  void startZoom();
-  void endZoom();
-  void startTranslation();
-  void endTranslation();
-  void startRotation();
-  void endRotation();
-
-
 public:
-  float final_tx;
-  float final_ty;
-  float final_tz;
-
-  float final_rd;
-  float final_rx;
-  float final_ry;
-  float final_rz;
-
   // cam stuff
   float cam_x = -10;
   float cam_y = -10; 
@@ -83,11 +33,8 @@ public:
   ~UI() {};
 
   void motionFunction(const int x, const int y);
-  void mouseFunction(int button, int state, int x, int y);
   void keyFunction(GLubyte key, GLint x, GLint y);
-  void applyViewingTransformation();
   void specialFunction(int key, int x, int y);
-
 };
 
 
